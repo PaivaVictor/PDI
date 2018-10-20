7a9e86ac2859e20a57c00cba8ebf83f040e2552b
PaivaVictor <42010942+PaivaVictor@users.noreply.github.com>
2018-10-20 16:27:10 -0300

    finalização

--- Filtragem_no_Dominio_Espacial_II/tiltshift.cpp
+++ Filtragem_no_Dominio_Espacial_II/tiltshift.cpp
@@ -1,77 +1,118 @@
 #include <iostream>
+#include <stdio.h>
+#include <cmath>
 #include <opencv2/opencv.hpp>
-#include <opencv2/imgproc/imgproc.hpp>
+
 using namespace cv;
 using namespace std;
 
-double alpha, l1 = 20, l2 =30;
-int altura = 0;
-int altura_max = 100;
+double janela = 0;
+double alfa = 0;
+float alpha = 0;
+double decaimento = 0;
+double altura = 0;
+double pos1 = 0;
+double pos2 = 0;
 
-int decaimento = 1;
-int decaimento_max = 100;
+int width, height, width1, height1, width2, height2;
 
-int posVertical = 0;
-int posVertical_max = 100;
+int janela_slider = 0;
+int janela_slider_max = 100;
 
-Mat image1, image2, blended;
-Mat imageTop; 
+int altura_slider = 0;
+int altura_slider_max = 100;
 
+int decaimento_slider = 0;
+int decaimento_slider_max = 100;
 
-char TrackbarName[50];
+int pos1_slider = 0;
+int pos1_slider_max = 100;
 
-double alpha_x(){
+Mat image = imread("taj.jpg"), media(image.rows,image.cols,CV_32FC3);
+Mat tilt = Mat::zeros(image.rows,image.cols,CV_32FC3);
+Mat pond1 = Mat::zeros(image.rows,image.cols,CV_32FC3);
+Mat pond2 = Mat(image.rows,image.cols,CV_32FC3, Scalar(1,1,1)); 
+Mat shift1(image.rows,image.cols,CV_32FC3,Scalar(0,0,0)), shift2(image.rows,image.cols,CV_32FC3,Scalar(0,0,0));
+Vec3f foco, semfoco; 
 
+char TiltShift[50];
 
-	alpha = altura*(tanh((x+posVertical)/decaimento)-tanh((x-(posVertical+10))/decaimento))  // quanto maior o decaimento mais suave é a descida/subida. l1 e l2 mexem na posição de subida e descida, respectivamente.
+//calcula o coeficiente alfa para as matrizes de ponderação
+void ponderacao(int, void*){
 
-	return alpha;
+ altura = (double) altura_slider/(10*altura_slider_max);
+ decaimento = (double) decaimento_slider*100/decaimento_slider_max;
+ pos1 = (double) pos1_slider*(height/2)/pos1_slider_max;
+ janela = (double) janela_slider*100/janela_slider_max;
+ pos2 = pos1 + janela;
+
+	for(int x =0 ; x<height; x++){
+		alfa = 0.5*(tanh((x-pos1)/decaimento) -tanh((x-pos2)/decaimento));
+		for(int y =0; y<width; y++){
+			foco[0] = alfa;
+			semfoco[0] = 1-alfa;
+			foco[1] = alfa;
+			semfoco[1] = 1-alfa;
+			foco[2] = alfa;
+			semfoco[2] = 1- alfa;
+			pond1.at<Vec3f>(x,y) = foco;
+			pond2.at<Vec3f>(x,y) = semfoco;
+		}
+	}
+
+  multiply(image, pond1, shift1, 1, -1);
+
+  multiply(media, pond2, shift2, 1, -1);
+
+ tilt = shift1 + shift2;
+ tilt.convertTo(tilt, CV_8UC3);
+ imshow("TiltShifted", tilt); 
+
 }
 
-void on_trackbar_alpha(int, void*){
- alfa = (double) alfa_slider/alfa_slider_max ;
- addWeighted( image1, alfa, imageTop, 1-alfa, 0.0, blended);
- imshow("addweighted", blended);
-}
+int main(){
 
-void on_trackbar_decaimento(int, void*){
-  image1.copyTo(imageTop);
-  int limit = top_slider*255/100;
-  if(limit > 0){
-	Mat tmp = image2(Rect(0, 0, 256, limit));
+  width=image.size().width;
+  height=image.size().height;
 
-	tmp.copyTo(imageTop(Rect(0, 0, 256, limit)));
-  }
-  on_trackbar_blend(alfa_slider,0);
-}
+  image.convertTo(image, CV_32FC3);
 
-int main(int argvc, char** argv){
-  image1 = imread("blend1.jpg");
-  image2 = imread("blend2.jpg");
-  image2.copyTo(imageTop);
-  namedWindow("addweighted", 1);
+  medianBlur(image, media, 5);
+
+  namedWindow("TiltShifted", 1);
   
-  sprintf( TrackbarName, "Altura do centro x %d", altura_max );
-  createTrackbar( TrackbarName, "addweighted",
-				  &altura,
-				  altura_max,
-				  on_trackbar_alpha );
-  on_trackbar_alpha(alfa_slider, 0 );
-  
-  sprintf( TrackbarName, "Decaimento x %d", decaimento_max );
-  createTrackbar( TrackbarName, "addweighted",
-				  &decaimento,
-				  decaimento_max,
-				  on_trackbar_decaimento );
-  on_trackbar_decaimento(top_slider, 0 );
+  sprintf( TiltShift, "Altura x %d", altura_slider_max );
 
-  sprintf( TrackbarName, "Posição Vertical x %d", posVertical_max );
-  createTrackbar( TrackbarName, "addweighted",
-				  &posVertical,
-				  posVertical_max,
-				  on_trackbar_posVertical );
-  on_trackbar_posVertical(top_slider, 0 );
+  createTrackbar( TiltShift, "TiltShifted",
+				  &altura_slider,
+				  altura_slider_max,
+				  ponderacao );
+
+  ponderacao(altura_slider,0);
+
+ 
+  sprintf( TiltShift, "Decaimento x %d", decaimento_slider_max );
+  createTrackbar( TiltShift, "TiltShifted",
+				  &decaimento_slider,
+				  decaimento_slider_max,
+				  ponderacao );
+  ponderacao(decaimento_slider,0);
+  
+  sprintf( TiltShift, "Centro x %d", pos1_slider_max );
+  createTrackbar( TiltShift, "TiltShifted",
+				  &pos1_slider,
+				  pos1_slider_max,
+				  ponderacao  );
+  ponderacao(pos1_slider,0);
+
+  sprintf( TiltShift, "Janela x %d", janela_slider_max );
+  createTrackbar( TiltShift, "TiltShifted",
+				  &janela_slider,
+				  janela_slider_max,
+				  ponderacao  );
+  ponderacao(janela_slider,0);
 
   waitKey(0);
+  imwrite("foto.jpg",tilt);
   return 0;
 }
